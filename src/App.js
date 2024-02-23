6f1b7daebcdb6daedc44cb45321d6424a4eb8175
indeed <bhojrajpathak001@gmail.com>
2024-02-23 23:10:03 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Router, Route, Routes, BrowserRouter } from "react-router-dom";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
