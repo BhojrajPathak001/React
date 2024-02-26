078f9423f419ff26253001d58e9ddd6c4c8035bb
indeed <bhojrajpathak001@gmail.com>
2024-02-27 01:38:41 +0530

    changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
-  return <div>home</div>;
+  return (
+    <>
+      <div>
+        <h1>Home Page</h1>
+        <a href="/products">all products</a>
+        <Link to='/products'>prouducts</Link>
+      </div>
+    </>
+  );
 }
